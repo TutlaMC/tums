7d160c4f3264f713277bc69b6f042fa1f5d0b386
TutlaMC <ayaanibrahim1110@gmail.com>
2025-07-31 22:34:27 +0530

    ANOTHER fix

--- src/main/java/net/tutla/tums/tusan/nodes/effects/Wait.java
+++ src/main/java/net/tutla/tums/tusan/nodes/effects/Wait.java
@@ -12,7 +12,7 @@ public class Wait extends Node {
     public Wait create() {
         try {
             Double time = (Double) new Expression(interpreter.nextToken()).create().value;
-            Thread.sleep(time.intValue());
+            Thread.sleep(time.intValue()* 1000L);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
